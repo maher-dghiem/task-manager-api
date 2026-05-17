f7e97e399994d8458781455740091b922140d674
Maher Dghiem <maherda5.9@gmail.com>
2026-05-17 05:04:56 +0300

    fixed staff bug

--- task_manager_drf/accounts/serializers.py
+++ task_manager_drf/accounts/serializers.py
@@ -6,12 +6,13 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['id', 'username', 'email', 'password', 'is_staff']
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            is_staff = validated_data.get('is_staff', False)
         )
         return user
